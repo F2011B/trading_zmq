75ef65c6b7a789723d2a27b768455865487192be
Dark Light <darkshadow201710[at]gmx.de>
2018-10-23 19:22:59 +0200

    Cleaned subaverage.py to test codacity

--- python/subaverage.py
+++ python/subaverage.py
@@ -5,22 +5,22 @@ import collections
 
 port = "5556"
 if len(sys.argv) > 1:
-    port =  sys.argv[1]
+    port = sys.argv[1]
     int(port)
     
 if len(sys.argv) > 2:
-    port1 =  sys.argv[2]
+    port1 = sys.argv[2]
     int(port1)
 
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
-print( "Collecting updates from weather server...")
+print("Collecting updates from weather server...")
 socket.connect ("tcp://localhost:%s" % port)
 
 if len(sys.argv) > 2:
-    socket.connect ("tcp://localhost:%s" % port1)
+    socket.connect("tcp://localhost:%s" % port1)
     
 # Subscribe to zipcode, default is NYC, 10001
 socket.setsockopt_string(zmq.SUBSCRIBE, '')
@@ -30,11 +30,11 @@ total_value = 0
 
 
 
-avlist= collections.deque(maxlen=10)
+avlist = collections.deque(maxlen=10)
 
 
 while True:
     string = socket.recv().decode("utf-8", "strict")      
     if string.split()[0] == 'EUR_USD':
-        avlist.append( float(string.split()[2].split('/')[0]))
-        print(sum( i for i in avlist)/10)
+        avlist.append(float(string.split()[2].split('/')[0]))
+        print(sum(i for i in avlist)/10)
